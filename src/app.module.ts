f0f5cb9e5b900e3bd9e17dd963f0b716136fd88c
Swathikrishna-0 <ssspriyamoru@gmail.com>
2025-03-17 15:36:18 +0530

    final commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,7 +17,7 @@ import { ConfigModule } from '@nestjs/config';
       password: process.env.PG_PASSWORD,
       database: process.env.PG_DB,
       autoLoadEntities: true,
-      synchronize: true,
+      synchronize: false,
     }),
     TasksModule,
   ],
