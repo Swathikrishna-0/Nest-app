76f51e3612bee0d27ba67ffb34b2a72df9b3b648
Swathikrishna-0 <ssspriyamoru@gmail.com>
2025-03-17 17:03:22 +0530

    env added

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,19 +10,15 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot(),
     TasksModule,
     TypeOrmModule.forRoot({
-      type: process.env.DB_TYPE as any,
-      host: process.env.PG_HOST,
-      port: parseInt(process.env.PG_PORT || '5432', 10),
-      username: process.env.PG_USER,
-      password: process.env.PG_PASSWORD,
-      database: process.env.PG_DB,
-      url: process.env.DATABASE_URL, 
+      type: "postgres",
+      url: process.env.DATABASE_URL,
       autoLoadEntities: true,
       synchronize: false,
+      ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
     }),
     TasksModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
